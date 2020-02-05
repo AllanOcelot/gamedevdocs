c389492b44575f90b0e10829b665fd510f56b9ed
AllanCodes <workofcode@gmail.com>
2020-02-05 19:52:34 +0000

    Getting familiar with Nuxt a little bit.

---
+++ components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <header>
+      <div class="container">
+        <a href="/" class="main-logo">GameDevDocs</a>
+        <div class="search-container" />
+        <nav class="main-nav">
+          <ol>
+            <li>Posts</li>
+            <li>News</li>
+            <li>Account</li>
+          </ol>
+        </nav>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  header {
+    position: sticky;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    background: #e6e6e6;
+    color: #fff;
+  }
+
+  header .container {
+    display: block;
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+
+  header .main-logo {
+    float: left;
+    padding: 10px 0;
+    cursor: pointer;
+  }
+
+  nav {
+    float: right;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="main-body-container">
+      <div class="container">
+      <div class="block">
+        <div class="image-container">
+          <img src="https://loremflickr.com/340/340" />
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  body {
+    background: #666;
+    min-height: calc(100vh - 50px);
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
+  }
+
+</style>
